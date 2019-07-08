bef19b7f8664f071a6ac68b77a5b83c90b334fe5
Joseph <firepheonix1914@gmail.com>
2019-07-08 09:38:02 -0400

    server is now running on port 5000

--- API.js
+++ API.js
@@ -3,17 +3,8 @@ const express = require('express');
 // calling express and strong its value in a variable
 const app = express();
 
-// routing any traphic going to the root url and sending it back to a landing page.
-app.get('/', function(request, response){
-    response.send("Landing page!")
-});
-// rerouting any traphic thats going to /spooky and sending back a response.
-app.get('/spooky', function(req, res){
-    res.send("you are in my spooky page")    
-});
-
 // our application is listening for request on port:3000
-app.listen(3000);
+app.listen(5000);
 
 // letting us know that our server is up. if it didnt pop up it lets you know your server is not running
-console.log("Server is live on port 3000....");
+console.log("Server is live on port 5000....");
